456168fc0b620598ca282d9be00d1d57b0ee30c2
sunil meher <sunilmeherraxa09@gmail.com>
2023-09-17 13:11:39 +0530

    Update Main.java

--- G3B4_FOP_GradedAssignment1/Department/src/com/greatLearning/department/Main/Main.java
+++ G3B4_FOP_GradedAssignment1/Department/src/com/greatLearning/department/Main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		AdminDepartment admin = new AdminDepartment();
+	AdminDepartment admin = new AdminDepartment();
         HrDepartment hr = new HrDepartment();
         TechDepartment tech = new TechDepartment();
 
