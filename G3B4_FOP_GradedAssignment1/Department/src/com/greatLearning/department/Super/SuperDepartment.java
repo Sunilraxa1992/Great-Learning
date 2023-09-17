1a05b01c12932b12951a7d75829a3730f5cba77f
sunil meher <sunilmeherraxa09@gmail.com>
2023-09-17 13:10:08 +0530

    Update SuperDepartment.java

--- G3B4_FOP_GradedAssignment1/Department/src/com/greatLearning/department/Super/SuperDepartment.java
+++ G3B4_FOP_GradedAssignment1/Department/src/com/greatLearning/department/Super/SuperDepartment.java
@@ -4,25 +4,21 @@ public class SuperDepartment implements IDepartment {
 
 	@Override
 	public String departmentName() {
-		// TODO Auto-generated method stub
 		return "Super Department";
 	}
 
 	@Override
 	public String getTodaysWork() {
-		// TODO Auto-generated method stub
 		return "No work as of now";
 	}
 
 	@Override
 	public String getWorkDeadline() {
-		// TODO Auto-generated method stub
 		return "Nil";
 	}
 
 	@Override
 	public String isTodayAHoliday() {
-		// TODO Auto-generated method stub
 		return "Today is not a Holiday";
 	}
 	
