90eaed21dea88dcff65297ce479e77b4e7d8fb87
sunil meher <sunilmeherraxa09@gmail.com>
2023-09-17 12:48:51 +0530

    Add files via upload

---
+++ G3B4_FOP_GradedAssignment1/Department/src/com/greatLearning/department/Super/IDepartment.java
@@ -0,0 +1,9 @@
+package com.greatLearning.department.Super;
+
+public interface IDepartment {
+
+	String departmentName();
+	String getTodaysWork();
+	String getWorkDeadline();
+	String isTodayAHoliday();
+}
